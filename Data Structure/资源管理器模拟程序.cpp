a3b83fa9d3d85a0f2a957bffcce910aeb01e5aca
WangXing365 <30504669+WangXing365@users.noreply.github.com>
2021-05-24 15:10:57 +0800

    over

--- Data Structure/资源管理器模拟程序.cpp
+++ Data Structure/资源管理器模拟程序.cpp
@@ -215,3 +215,120 @@ void insert(PTreeNode T)
 	else printf("no parent data.\n");
 }
 //------------删除指定结点-------------
+int findparent(PTreeNode T,char name[20],PTreeNode *parent,PTreeNode *pre,PTreeNode *pthis)
+{
+	if(T)
+	{
+		if(strcmp(T->wjm,name)==0)
+		{
+			*pre=*parent;
+			*pthis=T;
+			return 1;
+		}
+		else if(findparent(T->firstchild,name,&T,pre,pthis))
+		return 1;
+		else if(findparent(T->brother,name,&T,pre,pthis))
+		return 1;
+	}
+	else return 0;
+}
+void destroy(PTreeNode T)
+{
+	if(T==NULL)
+	return;
+	if(T->firstchild)
+	destroy(T->firstchild);
+	if(T->brother)
+	destroy(T->brother);
+	delete(T);
+}
+void delet(PTreeNode T)
+{
+	char node[20];
+	PTreeNode parent,pthis,pre;
+	parent=pthis=pre=NULL;
+	printf("input node to delete：\n");
+	scanf("%s",node);
+	if(findparent(T,node,&parent,&pre,&pthis))
+	{
+		if(pre==NULL)
+		destroy(T);
+		if(pre->firstchild==pthis)
+		{
+			pre->firstchild=pthis->brother;
+			delete(pthis);
+		}
+		else
+		{
+			pre->brother=pthis->brother;
+			delete(pthis);
+		}
+	}else printf("no data. \n");
+}
+void node(PTreeNode T)
+{
+	while(T)
+	{
+		if(T->firstchild==NULL)
+		printf("%s",T->wjm);
+		else
+		{
+			yesijiedian(T->firstchild);
+		}
+		T=T->brother;
+	}
+}
+void main()
+{
+	PTreeNode T;
+	PStack S;
+	CreatStack(&S);
+	int choice;
+	char name[20];
+	while(1)
+	{
+		printf("1---按边创建树\n");
+		printf("2---按先序遍历字符串创建二叉树\n");
+		printf("3---显示目录树\n");
+		printf("4---显示指定文件所在的全路径\n");
+		printf("5---插入\n");
+		printf("6---删除\n");
+		printf("7---销毁\n");
+		printf("0---退出\n");
+		printf("请输入选项：\n");
+		scanf("%d",&choice);
+		switch(choice)
+		{
+			case 1:
+				printf("按边输入树：\n");
+				CreatTree1(&T);
+				break;
+			case 2:
+			    printf("按对应二叉树输入树：\n");
+				CreatTree2(&T);
+				break;
+			case 3:
+			    display(T,0);
+				break;
+			case 4:
+			    printf("input name：\n");
+				scanf("%s",name);
+				path(T,name,S);
+				break;
+			case 5:
+				insert(T);
+				break;
+			case 6:
+				delet(T);
+				break;
+			case 7:
+				destroy(T);
+				break;
+			case 8:
+				node(T);
+				break;
+			case 0:
+				break;
+		}
+	}
+}
